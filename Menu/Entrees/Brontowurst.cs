91e1bd46444669fe26d44b0925ab345f7916bacb
Samuel Brunner <sbrunner5124@ksu.edu>
2019-09-13 14:34:48 -0500

    Saving

---
+++ Menu/Entrees/Brontowurst.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class Brontowurst
+    {
+        private bool bun = true;
+        private bool pepper = true;
+        private bool onion = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Brautwurst" };
+                if (bun) ingredients.Add("Whole Wheat Bun");
+                if (onion) ingredients.Add("Onion");
+                if (pepper) ingredients.Add("Peppers");
+                return ingredients;
+            }
+        }
+
+        public Brontowurst()
+        {
+            this.Price = 5.36;
+            this.Calories = 498;
+        }
+
+        public void HoldPeppers()
+        {
+            this.pepper = false;
+        }
+
+        public void HoldOnion()
+        {
+            this.onion = false;
+        }
+
+        public void HoldBun()
+        {
+            this.bun = false;
+        }
+    }
+}
+
+
---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        private int nuggetCount { get; set; }
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>();
+                for(int i=0; i< nuggetCount; i++)
+                {
+                    ingredients.Add("Chicken Nugget");
+                }
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.nuggetCount = 6;
+            this.Price = 4.25;
+            this.Calories = (59*nuggetCount);            
+        }
+
+        public void AddNugget()
+        {
+            this.nuggetCount++;
+            this.Price += 0.25;
+            this.Calories += 59;
+            this.Ingredients.Add("Chicken Nugget");
+        }
+
+        
+        
+    }
+}
+    
