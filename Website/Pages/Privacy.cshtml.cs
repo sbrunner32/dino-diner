2baf40e1e5f29e4f5b4159ef0208a0e322aab502
Samuel Brunner <sbrunner5124@ksu.edu>
2019-11-19 21:14:40 -0600

    Finished website milestone 1

--- Website/Pages/Privacy.cshtml.cs
+++ Website/Pages/Privacy.cshtml.cs
@@ -4,22 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using DinoDiner.Menu;
 
 namespace Website.Pages
 {
-    public class MenuPageModel : PageModel
+    public class PrivacyModel : PageModel
     {
-        private Menu _menu = new Menu();
-
-        public Menu Menu
-        {
-            get
-            {
-                return _menu;
-            }
-        }
-
         public void OnGet()
         {
 
